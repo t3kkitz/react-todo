488dd4755b07d20d0c56df197fa617ede01fdabb
@t3kkitz Sergey Bulgakov <t3kkitz@gmail.com>
2017-09-11 16:19:19 +0300

    add TodoItem

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import TodoListItem from './TodoListItem'
+import TodoItem from './TodoItem'
 
 export function TodoList(props) {
   return (
     <div className="Todo-List">
       <ul>
-        {props.todos.map(todo => <TodoListItem key={todo.id} todo={todo}/>)}
+        {props.todos.map(todo => <TodoItem key={todo.id} todo={todo}/>)}
       </ul>
     </div>
   );
